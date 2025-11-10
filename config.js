18b0ba663c3c1dabb34944a5075eb8af18ac1d22
ctlpowercmr-tech <ctlpowercmr@gmail.com>
2025-11-10 09:22:41 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 const CONFIG = {
-  API_URL: 'hhttps://server-12-thrn.onrender.com'
+  API_URL: 'https://server-12-thrn.onrender.com'
 };
 
 const OPERATEURS = [
   { id: 'orange', nom: 'Orange Money', icone: '🟠', couleur: '#FF6600' },
   { id: 'mtn', nom: 'MTN Mobile Money', icone: '🟡', couleur: '#FFCC00' }
 ];
+
 
 
 
